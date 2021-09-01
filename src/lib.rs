0e0647a730df0fb9cf823122e48201f631bf0cc0
Taiki Endo <te316e89@gmail.com>
2021-09-01 21:40:41 +0900

    Unify unordered list symbol in markdown

--- src/lib.rs
+++ src/lib.rs
@@ -87,10 +87,10 @@
 //!
 //! # Optional features
 //!
-//! * **`futures03`** — Implements [futures v0.3][futures03] traits for assert-unmoved types.
-//! * **`tokio1`** — Implements [tokio v1][tokio1] traits for assert-unmoved types.
-//! * **`tokio03`** — Implements [tokio v0.3][tokio03] traits for assert-unmoved types.
-//! * **`tokio02`** — Implements [tokio v0.2][tokio02] traits for assert-unmoved types.
+//! - **`futures03`** — Implements [futures v0.3][futures03] traits for assert-unmoved types.
+//! - **`tokio1`** — Implements [tokio v1][tokio1] traits for assert-unmoved types.
+//! - **`tokio03`** — Implements [tokio v0.3][tokio03] traits for assert-unmoved types.
+//! - **`tokio02`** — Implements [tokio v0.2][tokio02] traits for assert-unmoved types.
 //!
 //! [`Pin::new_unchecked`]: std::pin::Pin::new_unchecked
 //! [`StreamExt::next`]: https://docs.rs/futures/0.3/futures/stream/trait.StreamExt.html#method.next
