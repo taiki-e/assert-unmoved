7172e32e22df95bf27ddf1c9c50d6029cb2bd8a6
Taiki Endo <te316e89@gmail.com>
2023-03-10 00:07:57 +0900

    Enable clippy::as_ptr_cast_mut lint

--- src/lib.rs
+++ src/lib.rs
@@ -135,6 +135,7 @@ let _ = pinned_next.as_mut().poll(&mut cx).is_pending();
     clippy::std_instead_of_alloc,
     clippy::std_instead_of_core,
     // lints that help writing unsafe code
+    clippy::as_ptr_cast_mut,
     clippy::default_union_representation,
     clippy::trailing_empty_array,
     clippy::transmute_undefined_repr,
