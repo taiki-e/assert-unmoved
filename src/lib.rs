3247bdd70217b93b36204c62daa5bd9c7c4c847e
Taiki Endo <te316e89@gmail.com>
2024-01-06 11:32:22 +0900

    Apply missing_debug_implementations lint at workspace level

--- src/lib.rs
+++ src/lib.rs
@@ -120,7 +120,6 @@ Note: The MSRV when these features are enabled depends on the MSRV of these crat
 #![forbid(unsafe_code)]
 #![warn(
     // Lints that may help when writing public library.
-    missing_debug_implementations,
     missing_docs,
     clippy::alloc_instead_of_core,
     clippy::exhaustive_enums,
