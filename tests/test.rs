7537aeff3cbd6be271c0ab000db7cdd55487b838
Taiki Endo <te316e89@gmail.com>
2025-02-01 10:02:07 +0900

    tests: Update size test

--- tests/test.rs
+++ tests/test.rs
@@ -15,7 +15,7 @@ use futures::task::noop_waker;
 // Test the size of public types. This is not intended to keep a specific size and
 // is intended to be used only as a help in optimization.
 #[test]
-#[cfg_attr(any(not(target_pointer_width = "64"), miri), ignore)] // We set -Z randomize-layout for Miri.
+#[cfg_attr(any(not(target_pointer_width = "64"), miri, careful), ignore)] // We set -Z randomize-layout for Miri/cargo-careful.
 fn size() {
     assert_eq!(mem::size_of::<AssertUnmoved<Pending<()>>>(), 16);
 }
